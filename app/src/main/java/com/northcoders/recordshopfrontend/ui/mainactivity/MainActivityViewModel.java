8ed5a2feecf55b8f4a33d3dd8b71f6a797b97d54
Abulaf1a <peterrazzell688@gmail.com>
2024-12-17 09:25:43 +0000

    feat: implement MainActivity ViewModel

---
+++ app/src/main/java/com/northcoders/recordshopfrontend/ui/mainactivity/MainActivityViewModel.java
@@ -0,0 +1,28 @@
+package com.northcoders.recordshopfrontend.ui.mainactivity;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import com.northcoders.recordshopfrontend.model.Album;
+import com.northcoders.recordshopfrontend.model.AlbumRepository;
+
+import java.util.List;
+
+public class MainActivityViewModel extends AndroidViewModel {
+
+    //viewmodel gets data from repository
+    private AlbumRepository albumRepository;
+    public MainActivityViewModel(@NonNull Application application) {
+        super(application);
+        this.albumRepository = new AlbumRepository(application);
+    }
+
+    //this caches the data so if there are any config changes, it doesnt need to make
+    //another api call.
+    public LiveData<List<Album>> getAllAlbums(){
+        return albumRepository.getMutableLiveData();
+    }
+}
